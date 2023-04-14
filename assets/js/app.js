a51325b61de06ec5c8b7a3391a1dce9130810a03
ArannaBaura1971 <arannabaruase71@gmail.com>
2023-04-15 01:11:16 +0600

    updated and completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,28 @@
 // mouse hover affect in line 30 (id::add_note)
-const add_note_btn=document.getElementById('add_note');
+const add_note=document.getElementById('add_note');
 const icon=document.getElementById('icon');
 
-// add_note_btn.addEventListener('mouseenter',()=>{
+// add_note.addEventListener('mouseenter',()=>{
 //     icon.className='fa fa-arrow-right';
 // })
-// add_note_btn.addEventListener('mouseleave',()=>{
+// add_note.addEventListener('mouseleave',()=>{
 //     icon.className='fa fa-angle-right';
 // })
+const storeInlocalStorage=()=>{
+    const textarea =document.querySelector('.note_section').querySelectorAll('textarea');
+    let data=[];
+    console.log(textarea);
+    textarea.forEach((ele)=>{
+        data.push(ele.value);
+    })
+    console.log(data);
+
+    localStorage.setItem('notes',JSON.stringify(data));
+}
 
 // add note
 
-const Notes= ()=>{
-    let text='write something...';
+const Notes=(text)=>{
     const note= document.createElement('div');
     note.classList.add('note');
     let noteHtml=`
@@ -21,7 +31,7 @@ const Notes= ()=>{
             <button class="delete btn btn-danger"><i class="fas fa-trash-alt"></i></button>
         </div>
         <div class="main ${text? "":"hidden"}"></div>
-        <textarea class="${text? "hidden":""} "></textarea>
+        <textarea class="para ${text? "hidden":""} "></textarea>
     `
     note.insertAdjacentHTML('afterbegin',noteHtml);
     document.body.querySelector('.note_section').appendChild(note);
@@ -29,14 +39,16 @@ const Notes= ()=>{
     const edit=note.querySelector('.edit')
     const del=note.querySelector('.delete')
     const mainDiv=note.querySelector('.main')
-    const textarea=note.querySelector('textarea')
+    const textarea=note.querySelector('.para')
     
     del.addEventListener('click',()=>{
         note.remove();
+        storeInlocalStorage();
     })
 
     textarea.value=text;
     mainDiv.innerHTML=text;
+    console.log(text);
 
     edit.addEventListener('click',()=>{
         mainDiv.classList.toggle('hidden');
@@ -46,7 +58,12 @@ const Notes= ()=>{
     textarea.addEventListener('change',(event)=>{
         const value =event.target.value;
         mainDiv.innerHTML=value;
+        storeInlocalStorage();
     })
 }
 
-add_note_btn.addEventListener('click',Notes);
+const notes=JSON.parse(localStorage.getItem('notes'));
+
+notes.forEach((ele)=> Notes(ele))
+
+add_note.addEventListener('click',Notes);
